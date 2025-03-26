133a220bbcf363910a12d9a977fa8aa87294cf2a
UmarShabbirDev <umarshabbir957@gmail.com>
2025-03-26 10:41:02 +0100

    LLM Model added for retrieval

--- backend/pdf_utils.py
+++ backend/pdf_utils.py
@@ -3,6 +3,14 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+from openai import OpenAI
+from langchain.llms import OpenAI as LangOpenAI
+from langchain.chains import RetrievalQA
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def save_pdf_and_load(file_bytes):
     with tempfile.NamedTemporaryFile(delete=False, suffix='pdf') as tmp: 
@@ -26,3 +34,9 @@ def embed_store_chunks(documents):
     embeddings = HuggingFaceEmbeddings()
     db = FAISS.from_documents(documents,embeddings)
     return db
+
+def process_model(db):
+    llm = LangOpenAI(temperature=0,openai_api_key=os.getenv("OPENAI_API_KEY"))
+    retriever = db.as_retriever()
+    qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    return qa
